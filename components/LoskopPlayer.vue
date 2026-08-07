1a6c206f77b3c91626b6ed7641d6483973413f4d
Dimos Dimakakos <dimos@stinpriza.org>
2026-08-07 12:20:35 +0300

    Add: more styling changes

--- components/LoskopPlayer.vue
+++ components/LoskopPlayer.vue
@@ -29,7 +29,7 @@
     <div class="title">
       <template v-if="show.live">
         <span class="live-track-title">
-          {{ currentSong?.title ? `${currentSong.title}` : show.title }}
+          {{ currentSong?.artist ? `${currentSong.title}` : show.title }}
         </span>
       </template>
       <template v-else>
