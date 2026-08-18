22c08f8cacf0547e1df6c1861c3421bf1169f509
Dimos Dimakakos <dimos@stinpriza.org>
2026-08-18 19:49:51 +0300

    Add: update links to new cms

--- assets/constants.ts
+++ assets/constants.ts
@@ -1,3 +1,3 @@
-export const assets: string = "https://cms.loskop.radio/assets/";
+export const assets: string = "https://locms.stinpriza.eu/assets/";
 export const localAssets: string = "assets/";
-export const endpoint: string = "https://cms.loskop.radio/graphql";
+export const endpoint: string = "https://locms.stinpriza.eu/graphql";
